14df4e212ac484c9e1e3a8d8f881eb77b2a4802a
Yuhan Zhang <yuhan429@seas.upenn.edu>
2024-06-18 02:29:26 -0700

    logout

--- org/UserInterface.java
+++ org/UserInterface.java
@@ -1,9 +1,8 @@
 import java.time.ZonedDateTime;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,7 +11,7 @@ public class UserInterface {
 
 
     private final DataManager dataManager;
-    private final Organization org;
+    private Organization org;
     private final Scanner in = new Scanner(System.in);
 
     public UserInterface(DataManager dataManager, Organization org) {
@@ -20,27 +19,41 @@ public class UserInterface {
         this.org = org;
     }
 
+    private void displayOrganization() {
+        // display info of org
+        if (org.getFunds().size() > 0) {
+            System.out.println("There are " + org.getFunds().size() + " funds in this organization:");
+
+            int count = 1;
+            for (Fund f : org.getFunds()) {
+
+                System.out.println(count + ": " + f.getName());
+
+                count++;
+            }
+            System.out.println("Enter the fund number to see more information.");
+        }
+    }
+
+    // user's operation
     public void start() {
 
         while (true) {
-            System.out.println("\n\n");
-            if (org.getFunds().size() > 0) {
-                System.out.println("There are " + org.getFunds().size() + " funds in this organization:");
-
-                int count = 1;
-                for (Fund f : org.getFunds()) {
-
-                    System.out.println(count + ": " + f.getName());
-
-                    count++;
-                }
-                System.out.println("Enter the fund number to see more information.");
+            while (org == null) {
+                loginUI();
             }
+
+            System.out.println("\n\n");
+
+            displayOrganization();
+
+            // print options
             System.out.println("Enter 0 to create a new fund");
             // task 2.9
             System.out.println("Enter 'a' to view all contributions across all funds.");
             System.out.println("Enter 'q' or 'quit' to exit");
-            
+            System.out.println("Enter 'logout' to log out");
+
 
             String input = in.nextLine();
             if (input.equals("q") || input.equals("quit")) {
@@ -49,8 +62,12 @@ public class UserInterface {
             } else if (input.equalsIgnoreCase("a")) {
                 displayAllContributions();
                 continue;
+            } else if (input.equals("logout")) {
+                org = null;
+                continue;
             }
 
+            // display after log in
             try {
                 int option = Integer.parseInt(input);
                 if (option < 0 || option > org.getFunds().size()) {
@@ -67,8 +84,6 @@ public class UserInterface {
 
     }
 
-    
-    
     public void createFund() {
         while (true) {
             System.out.print("Enter the fund name: ");
@@ -108,8 +123,9 @@ public class UserInterface {
 
     private void waitForEnter() {
         System.out.println("Press the Enter key to go back to the listing of funds.");
-        in.nextLine();  
+        in.nextLine();
     }
+
     private void displayAllContributions() {
         System.out.println("\nAll Contributions Across All Funds:");
 
@@ -134,64 +150,7 @@ public class UserInterface {
         }
         waitForEnter();
     }
-    
 
-    
-//    // task 2.3
-//    private void calculateAggregates(Fund fund) {
-//        if (!fund.getAggregatedDonations().isEmpty()) return; // Already calculated
-//
-//        List<Donation> donations = fund.getDonations();
-//        Map<String, AggregateDonation> aggregated = new HashMap<>();
-//
-//        for (Donation donation :2 donations) {
-//            AggregateDonation agg = aggregated.getOrDefault(donation.getContributorName(),
-//                new AggregateDonation(0, 0));
-//            agg.incrementCount();
-//            agg.addToTotal(donation.getAmount());
-//            aggregated.put(donation.getContributorName(), agg);
-//        }
-//
-//        fund.getAggregatedDonations().putAll(aggregated);
-//    }
-//    
-    
-  /*
-	// task 4+10
-	public void displayFund(int fundNumber) {
-		Fund fund = org.getFunds().get(fundNumber - 1);
-		
-		System.out.println("\n\n");
-		System.out.println("Here is information about this fund:");
-		System.out.println("Name: " + fund.getName());
-		System.out.println("Description: " + fund.getDescription());
-		System.out.println("Target: $" + fund.getTarget());
-		
-		List<Donation> donations = fund.getDonations();
-		System.out.println("Number of donations: " + donations.size());
-	
-		long totalDonations = 0;
-		//task 10
-		DateTimeFormatter inputFormatter = DateTimeFormatter.ISO_DATE_TIME;
-		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
-		
-		// task 4
-		for (Donation donation : donations) {
-			totalDonations += donation.getAmount();
-			ZonedDateTime parsedDate = ZonedDateTime.parse(donation.getDate(), inputFormatter);
-			String formattedDate = parsedDate.format(outputFormatter);
-			System.out.println("* " + donation.getContributorName() + ": $" + donation.getAmount() + " on " + formattedDate);
-		}
-	
-		double percentageOfTarget = (double) totalDonations / fund.getTarget() * 100;
-		System.out.printf("Total donation amount: $%d (%.2f%% of target)\n", totalDonations, percentageOfTarget);
-	
-		System.out.println("Press the Enter key to go back to the listing of funds");
-		in.nextLine();
-	}
-
-    */
-    
     public void displayFund(int fundNumber) {
         Fund fund = org.getFunds().get(fundNumber - 1);
 
@@ -214,18 +173,18 @@ public class UserInterface {
 
         if (displayOption == 1) {
             // original code
-    		for (Donation donation : donations) {
-    			totalDonations += donation.getAmount();
-    			ZonedDateTime parsedDate = ZonedDateTime.parse(donation.getDate(), inputFormatter);
-    			String formattedDate = parsedDate.format(outputFormatter);
-    			System.out.println("* " + donation.getContributorName() + ": $" + donation.getAmount() + " on " + formattedDate);
-    		}
+            for (Donation donation : donations) {
+                totalDonations += donation.getAmount();
+                ZonedDateTime parsedDate = ZonedDateTime.parse(donation.getDate(), inputFormatter);
+                String formattedDate = parsedDate.format(outputFormatter);
+                System.out.println("* " + donation.getContributorName() + ": $" + donation.getAmount() + " on " + formattedDate);
+            }
         } else if (displayOption == 2) {
             Map<String, AggregateDonation> aggregates = fund.getAggregatedDonations();
 
             // Create a stream and sort in descending order
             Stream<Map.Entry<String, AggregateDonation>> sortedAggregatesStream = aggregates.entrySet().stream()
-                .sorted(Map.Entry.<String, AggregateDonation>comparingByValue((a, b) -> Long.compare(b.getTotal(), a.getTotal())));
+                    .sorted(Map.Entry.<String, AggregateDonation>comparingByValue((a, b) -> Long.compare(b.getTotal(), a.getTotal())));
 
             List<Map.Entry<String, AggregateDonation>> sortedAggregates = sortedAggregatesStream.collect(Collectors.toList());
 
@@ -241,29 +200,43 @@ public class UserInterface {
         System.out.println("Press the Enter key to go back to the listing of funds");
         in.nextLine();
     }
-    
-    
-    public static void main(String[] args) {
 
+    private void loginUI() {
+        System.out.println("enter login");
+        String login = in.nextLine();
+        System.out.println("enter password");
+        String password = in.nextLine();
+        Organization newOrg = attemptLogIn(login, password, dataManager);
+        if (newOrg != null) {
+            org = newOrg;
+        }
+    }
+
+    private static Organization attemptLogIn(String login, String password, DataManager ds) {
+
+        Organization org = null;
+        try {
+            org = ds.attemptLogin(login, password);
+        } catch (IllegalStateException e) {
+            System.out.println("Error in communicating with server");
+        }
+
+        if (org == null) {
+            // failed login
+            System.out.println("Login failed.");
+        }
+        return org;
+    }
+
+    public static void main(String[] args) {
         DataManager ds = new DataManager(new WebClient("localhost", 3001));
 
         String login = args[0];
         String password = args[1];
-        Organization org = null;
-        try {
-            org = ds.attemptLogin(login, password);
-        }catch (IllegalStateException e){
-            System.out.println("Error in communicating with server");
-        }
-        if (org == null) {
-            System.out.println("Login failed.");
-        } else {
+        Organization org = attemptLogIn(login, password, ds);
 
-            UserInterface ui = new UserInterface(ds, org);
-
-            ui.start();
-
-        }
+        UserInterface ui = new UserInterface(ds, org);
+        ui.start();
     }
 
 }
